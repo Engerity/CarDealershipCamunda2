7786a8e35f07db4091758aaef93d462df50c4bf6
Engerity <engerity@gmail.com>
2021-07-29 13:40:58 +0200

    Number of tasks displayed in the menu in badges

--- src/main/java/dealership/car/controller/TasksController.java
+++ src/main/java/dealership/car/controller/TasksController.java
@@ -2,10 +2,9 @@ package dealership.car.controller;
 
 import dealership.car.camunda.model.CompleteTasksModel;
 import dealership.car.camunda.model.CustomFormField;
-import dealership.car.model.Order;
-import dealership.car.model.OrderModel;
-import dealership.car.model.RoleEnum;
-import dealership.car.model.UserDetailsSecurity;
+import dealership.car.camunda.service.CamundaProcessService;
+import dealership.car.config.ProcessKey;
+import dealership.car.model.*;
 import org.apache.commons.lang3.StringUtils;
 import org.camunda.bpm.engine.form.FormField;
 import org.camunda.bpm.engine.form.TaskFormData;
@@ -30,24 +29,53 @@ import java.util.*;
 @RequestMapping("/tasks")
 public class TasksController extends AbstractController {
 
+    /**
+     * Obsługuje żądanie HTTP o ilosć tasków/zadań przypisanych do użytownika i do jego grupy
+     *
+     * @param userDetails dane zalogowanego użytkownika
+     * @return odpowiedź HTTP z ilością tasków/zddań przypisanych do użytownika i do jego grupy
+     */
+    @GetMapping("/count")
+    public ResponseEntity<?> getMyTasksNo(@AuthenticationPrincipal UserDetailsSecurity userDetails) {
+        // Taski przypisane do mnie
+        List<Task> assignedTasks = getTaskAssignedAndNotOrderConfiguration(userDetails.getUsername());
+
+        // Taski dla Konfiguracji zamówień
+        List<Task> forOrderConfig = getTaskForOrderConfigurationOnly(userDetails.getUsername(), camundaProcessService);
+
+        // Taski w grupach
+        Map<RoleEnum, List<Task>> taskInGroups = getTaskInGroups(userDetails.getAuthorities(), assignedTasks);
+
+        int countAssigneeToMe = assignedTasks.size();
+        int countInGroups = taskInGroups.values().stream().mapToInt(List::size).sum();
+        int countInOrderConfig = forOrderConfig.size();
+
+        return new ResponseEntity<>(new Integer[]{countAssigneeToMe, countInGroups, countInOrderConfig}, HttpStatus.OK);
+    }
+
+
+    /**
+     * Obsługuje wyświetlenie listy tasków/zadań
+     *
+     * @param userDetails       dane zalogowanego użytkownika
+     * @param completeTaskModel informacje o poszczególnych zadaniach na liście
+     * @param model             model danych przesyłany do widoku
+     * @return nazwa szablonu widoku
+     */
     @GetMapping()
     public String tasks(@AuthenticationPrincipal UserDetailsSecurity userDetails,
                         @ModelAttribute CompleteTasksModel completeTaskModel, Model model) {
-        List<Task> assignedTasks = camundaProcessService.getTasksForAssignee(userDetails.getUsername());
 
-        List<? extends GrantedAuthority> roles = new ArrayList<>(userDetails.getAuthorities());
+        // Taski przypisane do mnie
+        List<Task> assignedTasks = getTaskAssignedAndNotOrderConfiguration(userDetails.getUsername());
 
-        List<String> groups = new ArrayList<>();
-        roles.forEach(r -> groups.add(r.getAuthority()));
-        Map<RoleEnum, List<Task>> taskInGroups = new LinkedHashMap<>();
-        roles.forEach(r -> taskInGroups.put((RoleEnum) r, camundaProcessService.getTasksForCandidateGroupIn(Collections.singletonList(r.getAuthority()))));
+        // Taski w grupach
+        Map<RoleEnum, List<Task>> taskInGroups = getTaskInGroups(userDetails.getAuthorities(), assignedTasks);
 
-        taskInGroups.values().forEach(list -> list.removeIf(assignedTasks::contains));
-
-        Map<String, Order> tasksOrders = new HashMap<>();
         Set<Task> allTasks = new HashSet<>(assignedTasks);
         taskInGroups.forEach((key, value) -> allTasks.addAll(value));
 
+        Map<String, Order> tasksOrders = new HashMap<>();
         for (Task t : allTasks) {
             if (!tasksOrders.containsKey(t.getId())) {
                 String orderId = (String) camundaProcessService.getVariable(t.getProcessInstanceId(), "orderId");
@@ -64,6 +92,14 @@ public class TasksController extends AbstractController {
         return "tasks";
     }
 
+    /**
+     * Obsługa otwarcia obsługi/wykonania konkretnego zadania
+     *
+     * @param taskId      id zadania Camunda
+     * @param userDetails dane zalogowanego użytkownika
+     * @param model       model danych przesyłany do widoku
+     * @return nazwa szablonu widoku
+     */
     @GetMapping("/open/{taskId}")
     public String openTask(@PathVariable("taskId") String taskId, @AuthenticationPrincipal UserDetailsSecurity userDetails, Model model) {
         TaskFormData formData = camundaProcessService.getTaskFormData(taskId);
@@ -97,6 +133,14 @@ public class TasksController extends AbstractController {
         return formData.getFormKey();
     }
 
+    /**
+     * Obsługa otwarcia informacji o konkretnym zadaniu
+     *
+     * @param taskId      id zadania Camunda
+     * @param userDetails dane zalogowanego użytkownika
+     * @param model       model danych przesyłany do widoku
+     * @return nazwa szablonu widoku
+     */
     @GetMapping("/info/{taskId}")
     public String infoForTask(@PathVariable("taskId") String taskId, @AuthenticationPrincipal UserDetailsSecurity userDetails, Model model) {
         TaskFormData formData = camundaProcessService.getTaskFormData(taskId);
@@ -143,14 +187,29 @@ public class TasksController extends AbstractController {
         return "taskInfo";
     }
 
+    /**
+     * Obsługa przypisania zadania na siebie
+     *
+     * @param taskId      id zadania Camunda
+     * @param userDetails dane zalogowanego użytkownika
+     * @param model       model danych przesyłany do widoku
+     * @return przekierowanie na listę zadań
+     */
     @GetMapping("/assigne/{taskId}")
-    public ModelAndView setTaskAssigne(@PathVariable("taskId") String taskId, @AuthenticationPrincipal UserDetailsSecurity userDetails, Model model) {
+    public String setTaskAssigne(@PathVariable("taskId") String taskId, @AuthenticationPrincipal UserDetailsSecurity userDetails, Model model) {
         camundaProcessService.setTaskAssignee(taskId, userDetails);
-        return new ModelAndView("redirect:/tasks");
+        return "redirect:/tasks";
     }
 
+    /**
+     * Obsługa zatwierdzenia konkretnego zadania na liście zadań
+     *
+     * @param completeTaskModel dane obsługiwanego zadania
+     * @param redirect          obiekt informacji przekierowania
+     * @return przekierowanie na listę zadań
+     */
     @PostMapping()
-    public ModelAndView complete(@Valid CompleteTasksModel completeTaskModel, RedirectAttributes redirect) {
+    public String complete(@Valid CompleteTasksModel completeTaskModel, RedirectAttributes redirect) {
         List<CustomFormField> formValues = completeTaskModel.getFormValues();
 
         Map<String, Object> variables = new LinkedHashMap<>();
@@ -160,11 +219,11 @@ public class TasksController extends AbstractController {
             if (value == null || value instanceof String) {
 
                 if ("boolean".equals(customField.getTypeName())) {
-                    variables.put(id, Boolean.valueOf((String)value));
+                    variables.put(id, Boolean.valueOf((String) value));
                 } else {
                     variables.put(id, value);
                 }
-            } else  {
+            } else {
                 variables.put(id, value);
             }
         }
@@ -175,10 +234,16 @@ public class TasksController extends AbstractController {
             camundaProcessService.completeTask(completeTaskModel.getId(), variables);
 
         redirect.addFlashAttribute("globalMessage", "Pomyślnie ukończone zadanie  id " + completeTaskModel.getId() + ".");
-        return new ModelAndView("redirect:/tasks");
+        return "redirect:/tasks";
     }
 
-
+    /**
+     * Obsługa ządania HTTP o informacje nt. dostępności/konfiguracji przycisków na liscie zadań
+     *
+     * @param taskId     id zadania Camunda
+     * @param userDetail dane zalogowanego użytkownika
+     * @return odpowiedź HTTP z konfiguracją przycisków
+     */
     @GetMapping("/config/{taskId}")
     public ResponseEntity<?> getTaskConfig(@PathVariable("taskId") String taskId, @AuthenticationPrincipal UserDetailsSecurity userDetail) {
         Map<String, String> responseConfig = new HashMap<>();
@@ -198,8 +263,82 @@ public class TasksController extends AbstractController {
         return new ResponseEntity<>(responseConfig, HttpStatus.OK);
     }
 
+    /**
+     * Pomocnicza metoda sprawdzająca, czy dane formularza BPMN zawierają jakieś pola
+     *
+     * @param formData dane formularza BPMN
+     * @return true, jeśli formularz BPMN zawiera pola
+     */
     private boolean isFormDataAvailable(TaskFormData formData) {
         return formData != null && formData.getFormKey() != null && formData.getFormFields() != null && !formData.getFormFields().isEmpty();
     }
 
+    /**
+     * Pomocnicza metoda zwracająca mapę ról/uprawnień z odpowiadajacymi im zadaniami oczekującymi
+     *
+     * @param authorities   kolekcja uprawnień
+     * @param assignedTasks lista już przypisanych zadań do użytkownika (do wykluczenia w wyniku)
+     * @return mapa ról/uprawnień z odpowiadajacymi im zadaniami oczekującymi
+     */
+    private Map<RoleEnum, List<Task>> getTaskInGroups(Collection<? extends GrantedAuthority> authorities, List<Task> assignedTasks) {
+        List<? extends GrantedAuthority> roles = new ArrayList<>(authorities);
+
+        // Taski w grupach
+        Map<RoleEnum, List<Task>> taskInGroups = new LinkedHashMap<>();
+
+        //Klient nie widzi taskó innych klientów
+        if (roles.size() == 1 && roles.contains(RoleEnum.ROLE_CLIENT))
+            return taskInGroups;
+
+        roles.forEach(r -> taskInGroups.put((RoleEnum) r, camundaProcessService.getTasksForCandidateGroupIn(Collections.singletonList(r.getAuthority()))));
+        // ... ale nie przypisane do mnie
+        taskInGroups.values().forEach(list -> list.removeIf(assignedTasks::contains));
+
+        return taskInGroups;
+    }
+
+    /**
+     * Pomocnicza metoda zwracajaca listę zadań przypisanych do użytkownika (poza zadaniami z Konfiguratora zamówień)
+     *
+     * @param username nazwa zalogowanego użytkownika
+     * @return listę zadań przypisanych do użytkownika (poza zadaniami z Konfiguratora zamówień)
+     */
+    private List<Task> getTaskAssignedAndNotOrderConfiguration(String username) {
+        List<Task> assignedTasks = camundaProcessService.getTasksForAssignee(username);
+
+        // Usuwam z listy zadań te dla "Konfiguratora zamówień"
+        Iterator<Task> iterator = assignedTasks.iterator();
+        while (iterator.hasNext()) {
+            Task task = iterator.next();
+            if (task.getProcessDefinitionId().contains(ProcessKey.CAR_DEALERSHIP_KLIENT.name())) {
+                Object object = camundaProcessService.getVariable(task.getProcessInstanceId(), "orderData");
+                if (object instanceof OrderModel && StringUtils.isBlank(((OrderModel) object).getOrderId())) {
+                    iterator.remove();
+                }
+            }
+        }
+
+        return assignedTasks;
+    }
+
+    /**
+     * Pomocnicza metoda zwracajaca listę zadań przypisanych do użytkownika ale tylko dla Konfiguratora zamówień
+     *
+     * @param username nazwa zalogowanego użytkownika
+     * @return listę zadań przypisanych do użytkownika ale tylko dla Konfiguratora zamówień
+     */
+    public static List<Task> getTaskForOrderConfigurationOnly(String username, CamundaProcessService camundaProcessService) {
+        List<Task> result = new ArrayList<>();
+
+        for (Task task : camundaProcessService.getTasksForAssignee(username)) {
+            if (task.getProcessDefinitionId().contains(ProcessKey.CAR_DEALERSHIP_KLIENT.name())) {
+                Object object = camundaProcessService.getVariable(task.getProcessInstanceId(), "orderData");
+                if (object instanceof OrderModel && StringUtils.isBlank(((OrderModel) object).getOrderId())) {
+                    result.add(task);
+                }
+            }
+        }
+
+        return result;
+    }
 }
